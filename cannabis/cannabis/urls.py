34875077ed79a8b0ef88640184a3d258c66bbfb8
Leonardo Cavallucci <l.cavallucci@nephila.it>
2017-03-02 16:35:36 +0100

    Initial view for patient list

--- cannabis/cannabis/urls.py
+++ cannabis/cannabis/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from pazienti.views import PazienteListView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', PazienteListView.as_view(), name='paziente-list'),
 ]
--- cannabis/pazienti/views.py
+++ cannabis/pazienti/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Paziente
+
+
+class PazienteListView(ListView):
+
+    model = Paziente
+
+    def get_context_data(self, **kwargs):
+        context = super(PazienteListView, self).get_context_data(**kwargs)
+        return context
